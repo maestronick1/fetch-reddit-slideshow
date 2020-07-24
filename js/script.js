5d51c57bee4a53d7aaa083e37a69390dfa0f16b2
Nicholas Phillips <nicholasphillips@Nicholass-MacBook-Air.local>
2020-07-24 15:40:50 -0700

    Touch UP Js

--- js/script.js
+++ js/script.js
@@ -2,52 +2,68 @@ const form = document.querySelector('.container')
 const imageArray = document.querySelectorAll(".holder")
 const button = document.createElement("button");
     console.log(button);
-    button.classList.add('reset-button')
+    button.classList.add('reset-button');
+    button.textContent = 'Reset'
     form.appendChild(button);
-
-
-form.addEventListener("submit", function(e){
-    e.preventDefault()
-    const value = document.getElementById("input").value
-    console.log(value)
-    const url = `http://www.reddit.com/search.json?q=${value}+nsfw:no`//search bar function
-    console.log(url)
-    let response = fetch(url).then(function(response){
-        return response.json()
-        
-        
-        
-    }).then(function(data){
-        let thumbSearch = data.data.children;
-        console.log(data);
-        // if(data.blog === null);
-        // console.log('ther is not info here');
-        // if(data.thumbnail === '')
-        // console.log('here is something')
-        
-        for (let i = 0; i < thumbSearch.length; i++){
-            
-            let thumbNails = thumbSearch[i].data.thumbnail;
-            console.log(thumbNails);
-            console.log(imageArray);
-            imageArray[i].src = thumbNails
-            
-            
-            
-            
-            
-        }
-        
-    }).catch(error => {
-        console.log("this is an error" + error)
-    } )
+    
+   
+    // button.appendChild(text);
     
     
     
-    //console.log(response)
-})
-
-
-
-
-
+    resultsArray = []
+    
+    form.addEventListener("submit", function(e){
+        e.preventDefault()
+        const value = document.getElementById("input").value
+        console.log(value)
+        const url = `http://www.reddit.com/search.json?q=${value}+nsfw:no`//search bar function
+        console.log(url)
+        let response = fetch(url).then(function(response){
+            return response.json()
+            
+            
+            
+        }).then(function(data){
+            let thumbSearch = data.data.children;
+            console.log(data);
+            // if(data.blog === null);
+            // console.log('ther is not info here');
+            // if(data.thumbnail === '')
+            
+            // console.log('here is something')
+            
+            for (let i = 0; i < thumbSearch.length; i++){
+                
+                let thumbNails = thumbSearch[i].data.thumbnail;
+                console.log(thumbNails);
+                console.log(imageArray);
+                imageArray[i].src = thumbNails
+                
+                
+                
+                
+                
+                
+                
+            }
+            
+        }).catch(error => {
+            console.log("this is an error" + error)
+        })
+        
+        
+        
+        //console.log(response)
+    });
+    
+    resetButton.addEventListener("click", ()=>{
+        // hide slideshow
+    
+        // show search form
+        imageArray.style.display = "block"
+    })
+    
+    
+    
+    
